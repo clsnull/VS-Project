e8f82cd43b485c8b153c6e4bab27819dfb2d5465
lzf <1334084021@qq.com>
2026-01-14 18:32:43 +0800

    timber

--- Timber/Timber.cpp
+++ Timber/Timber.cpp
@@ -1,4 +1,5 @@
 ﻿#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <string>
 #include <iostream>
 #include <ctime>
@@ -44,16 +45,20 @@ int main()
     }
     sf::Text messageText(font);
     sf::Text scoreText(font);
+    sf::Text fpsText(font);
 
     messageText.setString("Press Enter to Start!");
     scoreText.setString("Score = 0");
+    fpsText.setString("FPS = 0");
 
     messageText.setCharacterSize(75);
     scoreText.setCharacterSize(100);
+    fpsText.setCharacterSize(14);
 
     messageText.setFillColor(sf::Color::White);
     scoreText.setFillColor(sf::Color::White);
-
+    fpsText.setFillColor(sf::Color::White);
+    
     scoreText.setPosition(sf::Vector2f(20, 20));
 
     sf::FloatRect textRect = messageText.getLocalBounds();
@@ -62,6 +67,7 @@ int main()
         textRect.position.y + textRect.size.y / 2.0f
     });
     messageText.setPosition({ WINDOW_WIDTH / 2.0f, WINDOW_HEIGHT / 2.0f });
+    fpsText.setPosition({ 3000, 20});
 
     sf::Texture textureBranch;
     textureBranch.loadFromFile("graphics/branch.png");
@@ -127,7 +133,59 @@ int main()
 
     sf::Clock clock;
 
+    sf::Texture texturePlayer;
+    texturePlayer.loadFromFile("graphics/player.png");
+    sf::Sprite spritePlayer(texturePlayer);
+    spritePlayer.setPosition(sf::Vector2f(580, 720));
+
+    side playerSide = side::LEFT;
+
+    sf::Texture textureRIP;
+    textureRIP.loadFromFile("graphics/rip.png");
+    sf::Sprite spriteRIP(textureRIP);
+    spriteRIP.setPosition(sf::Vector2f(600, 860));
+
+    sf::Texture textureAxe;
+    textureAxe.loadFromFile("graphics/axe.png");
+    sf::Sprite spriteAxe(textureAxe);
+    spriteAxe.setPosition(sf::Vector2f(700, 830));
+
+    const float AXE_POSITION_LEFT = 700;
+    const float AXE_POSITION_RIGHT = 1075;
+
+    sf::Texture textureLog;
+    textureLog.loadFromFile("graphics/log.png");
+    sf::Sprite spriteLog(textureLog);
+    spriteLog.setPosition(sf::Vector2f(810, 720));
+
+    bool logActive = false;
+    float logSpeedX = 1000;
+    float logSpeedY = -1500;
+
+    bool acceptInput = false;
+
+    sf::SoundBuffer chopBuffer;
+    chopBuffer.loadFromFile("sound/chop.wav");
+    sf::Sound chop(chopBuffer);
+
+    sf::SoundBuffer deathBuffer;
+    deathBuffer.loadFromFile("sound/chop.wav");
+    sf::Sound death(deathBuffer);
+        
+    sf::SoundBuffer ootBuffer;
+    ootBuffer.loadFromFile("sound/out_of_time.wav");
+    sf::Sound outOfTime(ootBuffer);
+
+    sf::Clock fpsClock;
     while (window.isOpen()) {
+        while (const std::optional event = window.pollEvent()) {
+            if (event -> is<sf::Event::KeyReleased>() && !paused) {
+                std::cout << "按键释放" << std::endl;
+                acceptInput = true;
+
+                spriteAxe.setPosition({ 2000, spriteAxe.getPosition().y });
+            }
+        }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape)) {
             window.close();
         }
@@ -136,8 +194,63 @@ int main()
             
             score = 0;
             timeRemaining = 6.0f;
+
+            for (int i = 0; i < NUM_BARNCHES; i++) {
+                branchPositions[i] = side::NONE;
+            }
+
+            spriteRIP.setPosition(sf::Vector2f(675, 2000));
+
+            spritePlayer.setPosition(sf::Vector2f(580, 720));
+
+            acceptInput = true;
         }
-        
+
+        if (acceptInput) {
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right)) {
+                std::cout << "按下右键" << std::endl;
+
+                playerSide = side::RIGHT;
+            
+                score++;
+
+                timeRemaining += (2 / score) + .15;
+                spriteAxe.setPosition(sf::Vector2f(AXE_POSITION_RIGHT, spriteAxe.getPosition().y));
+
+                spritePlayer.setPosition({ 1200, 720 });
+
+                updateBranches(score);
+
+                spriteLog.setPosition({ 810, 720 });
+                logSpeedX = -5000;
+                logActive = true;
+                acceptInput = false;
+
+                chop.play();
+            }
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)) {
+                std::cout << "按下左键" << std::endl;
+                playerSide = side::LEFT;
+
+                score++;
+
+                timeRemaining += (2 / score) + .15;
+                spriteAxe.setPosition(sf::Vector2f(AXE_POSITION_LEFT, spriteAxe.getPosition().y));
+
+                spritePlayer.setPosition({ 580, 720 });
+
+                updateBranches(score);
+
+                spriteLog.setPosition({ 810, 720 });
+                logSpeedX = 5000;
+                logActive = true;
+                acceptInput = false;
+
+                chop.play();
+            }
+        }
+
         window.clear();
 
         sf::Time dt = clock.restart();
@@ -157,6 +270,8 @@ int main()
                     textRect.position.y + textRect.size.y / 2.0f
                     });
                 messageText.setPosition({ WINDOW_WIDTH / 2.0f, WINDOW_HEIGHT / 2.0f });
+
+                outOfTime.play();
             }
 
             if (!beeActive) {
@@ -268,8 +383,52 @@ int main()
                     branches[i]->setPosition({ 3000, height });
                 }
             }
+            if (logActive) {
+                spriteLog.setPosition({
+                    spriteLog.getPosition().x + (logSpeedX * dt.asSeconds()),
+                    spriteLog.getPosition().y + (logSpeedY * dt.asSeconds())
+                });
+
+                if (spriteLog.getPosition().x < -100 || spriteLog.getPosition().x > 2000) {
+                    logActive = false;
+                    spriteLog.setPosition({810, 720});
+                }
+            }
+            if (branchPositions[5] == playerSide) {
+                paused = true;
+                acceptInput = false;
+                spriteRIP.setPosition({ 525, 760 });
+
+                spritePlayer.setPosition({ 2000, 660});
+
+                messageText.setString("SQUISHED!!!");
+
+                sf::FloatRect textRect = messageText.getLocalBounds();
+                messageText.setOrigin({
+                    textRect.position.x + textRect.size.x / 2.0f,
+                    textRect.position.y + textRect.size.y / 2.0f,
+                });
+                messageText.setPosition({ 1920 / 2.0f, 1080 / 2.0f });
+
+                death.play();
+            }
         }
-        
+
+        if (fpsClock.getElapsedTime().asSeconds() >= 1.0f) {
+            std::stringstream fps;
+            fps << "FPS = " << (1 / dt.asSeconds());
+            fpsText.setString(fps.str());
+
+            sf::FloatRect fpsRect = fpsText.getLocalBounds();
+            fpsText.setOrigin({
+                fpsRect.position.x + fpsRect.size.x,
+                fpsRect.position.y
+                });
+            fpsText.setPosition({ WINDOW_WIDTH - 10, 10 });
+
+            fpsClock.restart();
+        }
+
         window.draw(spriteBackground);
 
         window.draw(spriteCloud1);
@@ -283,10 +442,15 @@ int main()
         window.draw(spriteTree);
         window.draw(timeBar);
 
-        window.draw(spriteBee);
+        window.draw(spritePlayer);
+        window.draw(spriteAxe);
+        window.draw(spriteLog);
+        window.draw(spriteRIP);
+
         window.draw(spriteBee);
 
         window.draw(scoreText);
+        window.draw(fpsText);
 
         if (paused) {
             window.draw(messageText);
@@ -303,5 +467,20 @@ int main()
 void updateBranches(int seed) {
     for (int j = NUM_BARNCHES - 1; j > 0; j--) {
         branchPositions[j] = branchPositions[j - 1];
+    }
+
+    std::srand((int)time(0) + seed);
+    int r = (rand() % 5);
+
+    switch (r) {
+    case 0:
+        branchPositions[0] = side::LEFT;
+        break;
+    case 1:
+        branchPositions[0] = side::RIGHT;
+        break;
+    default:
+        branchPositions[0] = side::NONE;
+        break;
     }
 }
