71f71905498a6cfabfe8bb278be44fb20c49eff4
lzf <1334084021@qq.com>
2026-01-15 21:32:37 +0800

    Pong

---
+++ Pong/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+#include <SFML/Graphics.hpp>
+
+using namespace std;
+
+int main()
+{
+	sf::VideoMode vm({ 1920, 1080 });
+	sf::Window window(vm, "Pong");
+	while (window.isOpen()) {
+		window.display();
+	}
+	return 0;
+}
